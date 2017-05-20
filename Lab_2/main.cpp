60f1e01dab56c04627e8294ea1132612a8b80805
settnozz <settnozz@yandex.ua>
2017-05-20 16:22:49 +0300

    change lab 2

--- Lab_2/main.cpp
+++ Lab_2/main.cpp
@@ -2,9 +2,10 @@
 #include "Array_objects.h"
 
 int main() {
-    Array_objects *comps = new Array_objects;
+    Array_objects *comps = new Arrray_objects();
     comps->show_hdd_ozu(600, 1026);
     comps->show_no_dvd("No");
     comps->show_proc("Intel CORE i5");
+    delete comps;
     return 0;
 }
